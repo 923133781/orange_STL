f3c179deba2aae4bab1da562c83db9495a221a0b
Orange-Wu <44962400+923133781@users.noreply.github.com>
2020-12-02 10:32:51 +0800

    Enter

--- include/orange_list.h
+++ include/orange_list.h
@@ -130,6 +130,192 @@ struct list_iterator : public orange_stl::iterator<orange_stl::bidirectional_ite
     bool operator!=(const self &rhs) const { return node_!=rhs.node_; } 
 };
 
+template <class T> 
+struct list_const_iterator : public iterator<bidirectional_iterator_tag, T>
+{
+    typedef T                                   value_type;
+    typedef const T*                            pointer;
+    typedef const T&                            reference;
+    typedef typename node_traits<T>::base_ptr   base_ptr;
+    typedef typename node_traits<T>::node_ptr   node_ptr;
+    typedef list_const_iterator<T>              self;
+
+    base_ptr node_;
+
+    list_const_iterator() = default;
+    list_const_iterator(base_ptr x) : node_(x) {}
+    list_const_iterator(node_ptr x) : node_(x->as_base()) {}
+    list_const_iterator(const list_iterator<T> &rhs) : node_(rhs.node_){}
+    list_const_iterator(const list_const_iterator &rhs) : node_(rhs.node_) {}
+
+    reference   operator*()  const { return node_->as_node()->value; }
+    pointer     operator->() const { return &(operator*()); }
+
+    self& operator++()
+    {
+        ORANGE_STL_DEBUG(node_!=nullptr);
+        node_=node_->next;
+        return *this;
+    }
+    self operator++(int)
+    {
+        self tmp=*this;
+        ++*this;
+        return tmp;
+    }
+    self& operator--()
+    {
+        ORANGE_STL_DEBUG(node_!=nullptr);
+        node_=node_->prev;
+        return *this;
+    }
+    self operator--(int)
+    {
+        self tmp=*this;
+        --*this;
+        return tmp;
+    }
+
+    //重载比较操作符
+    bool operator==(const self &rhs) const { return node==rhs.node_; }
+    bool operator!=(const self &rhs) const { return node!=rhs.node_; }
+};
+
+/*  模板类 list
+    模板参数T表示数据类型 */
+template <class T>
+class list
+{
+public:
+    // 嵌套类型定义
+    typedef orange_stl::allocator<T>                        allocator_type;
+    typedef orange_stl::allocator<T>                        data_allocator;
+    typedef orange_stl::allocator<list_node_base<T>>        base_allocator;
+    typedef orange_stl::allocator<list_node<T>>             node_allocator;
+
+    typedef typename allocator_type::value_type             value_type;
+    typedef typename allocator_type::pointer                pointer;
+    typedef typename allocator_type::const_pointer          const_pointer;
+    typedef typename allocator_type::reference              reference;
+    typedef typename allocator_type::const_reference        const_reference;
+    typedef typename allocator_type::size_type              size_type;
+    typedef typename allocator_type::difference_type        difference_type;
+
+    typedef list_iterator<T>                                iterator;
+    typedef list_const_iterator<T>                          const_iterator;
+    typedef orange_stl::reverse_iterator<iterator>          reverse_iterator;
+    typedef orange_stl::reverse_iterator<const_iterator>    const_reverse_iterator;
+
+    typedef typename node_traits<T>::base_ptr               base_ptr;
+    typedef typename node_traits<T>::node_ptr               node_ptr;
+
+    allocator_type get_allocator()  { return node_allocator(); }
+
+private:
+    base_ptr    node_;  /* 指向末尾的节点 */
+    size_type   size_;  /* 大小 */ 
+
+public:
+    /* 构造、复制、移动、析构函数 */
+    list() { fill_init(0, value_type()); }
+    
+    explicit list(size_type n) { fill_init(n, value_type()); }
+    
+    list(size_type n, const T& value) { fill_init(n, value); }
+
+    template <class Iter, typename std::enable_if<orange_stl::is_input_iterator<Iter>::value, int>::type = 0>
+    list(Iter first, Iter last) { copy_init(first, last); } 
+
+    list(std::initializer_list<T> ilist)
+    { copy_init(ilist.begin(), ilist.end()); }
+
+    list(const list& rhs)
+    { copy_init(rhs.cbegin(), rhs.cend()); }
+
+    list(list &&rhs) noexcept : node_(rhs.node_), size_(rhs.size_)
+    { 
+        rhs.node_ = nullptr;
+        rhs.size_ = 0;
+    }
+
+    list& operator=(const list& rhs)
+    {
+        if(this != &rhs) assign(rhs.begin(), rhs.end());
+        return *this;
+    }
+
+    list& operator=(list&& rhs) noexcept
+    {
+        clear();
+        splice(end(), rhs);
+        return *this;
+    }
+
+    list& operator=(std::initializer_list<T> ilist)
+    {
+        list tmp(ilist.begin(), ilist.end());
+        swap(tmp);
+        return *this;
+    }
+
+    ~list()
+    {
+        if(node_)
+        {
+            clae();
+            base_allocator::deallocate(node_);
+            node_=nulllptr;
+            size_=0;
+        }
+    }
+
+public:
+    /* 迭代器的相关操作 */
+    iterator begin() noexcept { return node_->next; };
+    const_iterator begin() const noexcept { return node_->next; }
+    iterator end() noexcept {return node_; }
+    const_iterator end() const noexcept { return node_; }
+
+    reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+    const_reverse_iterator rbegin() const noexcept { return reverse_iterator(end()); }
+    reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
+    const_reverse_iterator rend() const noexcept { return reverse_iterator(begin()); }
+
+    const_iterator cbegin() const noexcept { return begin(); };
+    const_iterator cend() const noexcept { return end(); };
+    const_reverse_iterator crbegin() const noexcept { return rbegin(); }
+    const_reverse_iterator crend() const noexcept { return rend(); }
+
+    /* list容器容量相关函数  */
+    bool empty() const noexcept { return node_->next == node_; }
+
+    size_type size() const noexcept { return size_; }
+
+    size_type max_size() const noexcept { retrun static_cast<size_type>(-1); }
+
+    /* 访问相关的元素 */
+    reference front()
+    {
+        ORANGE_STL_DEBUG(!empty());
+        return *bengin();
+    }
+    const_reference front() const
+    {
+        ORANGE_STL_DEBUG(!empty());
+        return *begin();
+    }
+    reference back()
+    {
+        ORANGE_STL_DEBUG(!empty());
+        return *(--end());
+    }
+    const_reference back() const
+    {
+        ORANGE_STL_DEBUG(!empty());
+        return *(--end());
+    }
+
+};
 
 } // namespace orange_stl
 
