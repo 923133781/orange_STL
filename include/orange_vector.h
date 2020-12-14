e4c320242dcfa7e37f866b049480c91b2efac776
Orange-Wu <44962400+923133781@users.noreply.github.com>
2020-12-14 10:52:25 +0800

    Enter

--- include/orange_vector.h
+++ include/orange_vector.h
@@ -112,7 +112,7 @@ public:
     /* 容器容量 */
     bool empty() const noexcept { return begin_==end_; }
     size_type size() const noexcept { return static_cast<size_type>(end_-begin_); }
-    size_type maax_size() const noexcept { return static_cast<size_type>(-1)/sizeof(T); }
+    size_type max_size() const noexcept { return static_cast<size_type>(-1)/sizeof(T); }
     size_type capacity() const noexcept { return static_cast<size_type>(cap_-begin_); }
     void reserve(size_type n);
     void shrink_to_fit();
@@ -183,7 +183,7 @@ public:
     iterator emplace(const_iterator pos, Args&& ...args);
 
     template <class... Args>
-    iterator emplace_back(Args&& ...args);
+    void emplace_back(Args&& ...args);
 
     /* push_back   pop_back    */
     void push_back(const value_type& value);
@@ -258,7 +258,190 @@ private:
 
 };
 
+/* 赋值复制操作符 */
+template <class T>
+vector<T>& vector<T>::operator=(const vector& rhs)
+{
+    if(this!=&rhs)
+    {
+        const auto len=rhs.size();
+        if(len>capacity())/* rhs的大小比当前容器数量多 */
+        {
+            vector tmp(rhs.begin(), rhs.end());
+            swap(tmp);
+        }
+        else if(size()>=len)    /* 当前容器数据数量比比rhs多 */
+        {
+            auto i=orange_stl::copy(rhs.begin(), rhs.end(), begin());
+            data_allocator::destroy(i, end_);
+            end_=begin_+len;
+        }
+        else    /* len 比size()大且比 容量reserve()小 */
+        {
+            orange_stl::copy(rhs.begin(), rhs.begin()+size(), begin_); /* 将原有size()大小进行赋值 */
+            orange_stl::uninitialized_copy(rhs.begin()+size(), rhs.end(), end_);/* 目标区间未初始化，依次调用拷贝构造函数 */
+            cap_=end_=begin_+len;
+        }
+    }
+    return *this;
+}
 
+/* 移动赋值操作符 */
+template<class T>
+vector<T>& vector<T>::operator=(vector&& rhs) noexcept
+{
+    destroy_and_recover(begin_, end_, cap_-begin_);
+    begin_=rhs.begin_;
+    end_=rhs.end_;
+    cap_=rhs.cap_;
+    rhs.begin_=nullptr;
+    rhs.end_=nullptr;
+    rhs.cap_=nullptr;
+    return *this;
+}
+
+/* 预留空间大小，原容量小于要求的时候，才会重新进行分配 */
+template <class T>
+void vector<T>::reserve(size_type n)
+{
+    if(capacity() < n)
+    {
+        THROW_LENGTH_ERROR_IF(n > max_size(), "n can not larger than  max_size() int vector<T>::reserve(n)");
+        const auto old_size=size();
+        auto tmp=data_allocator::allocate(n);
+        orange_stl::uninitialized_move(begin_, end_, tmp);
+        begin_=tmp;
+        end_=tmp+old_size;
+        cap_=begin_+n;
+    }
+}
+
+/* 缩小当前容器容量 */
+template <class T>
+void vector<T>::shrink_to_fit()
+{
+    if(end_<cap_)
+    {
+        reinsert(size());
+    }
+}
+
+/* 在pos位置构造元素，避免额外的复制或移动的开销 */
+template <class T>
+template <class ...Args>
+typename vector<T>::iterator
+vector<T>::emplace(const_iterator pos, Args&& ...args)
+{
+    ORANGE_STL_DEBUG(pos>=begin() && pos<=end());
+    iterator xpos=const_cast<iterator>(pos);
+    const size_type n=xpos-begin_;
+    if(end_!=cap && xpos==end)
+    {
+        data_allocator::construct(orange_stl:::address_of(*end_), orange_stl::forward<Args>(args)...);
+        ++end_;
+    }
+    else if(end_!=cap_)
+    {
+        auto new_end=end_;
+        data_allocator::construct(orange_stl::address_of(*end_), *(end_-1));
+        ++new_end;
+        orange_stl::copy_backward(xpos, end_-1, end_);
+        *xpos=value_type(orange_stl::forward<Args>>(args)...);
+    }
+    else
+    {
+        reallocate_emplace(xpos, orange_stl::forward<Args>(args)...);
+    }
+    return begin()+n;
+}
+
+// 在尾部就地构造元素，避免额外的复制或移动开销
+template <class T>
+template <class ...Args>
+void vector<T>::emplace_back(Args&& ...args)
+{
+    if(end_<cap_)
+    {
+        data_allocator::construct(orange_stl::address_of(*end_), orange_stl::forward<Args>(args)...);
+        ++end_;
+    }
+    else
+    {
+        reallocate_emplace(end_, orange_stl::forward<Args>(args)...);
+    }
+}
+
+
+/* 重新分配空间，并且在pis处就地构造元素 */
+template <class T>
+template <class ...Args>
+void vector<T>::reallocate_emplace(iterator pos, Args&& ...args)
+{
+    const auto new_size = get_new_cap(1);
+    auto new_begin = data_allocator::allocate(new_size);
+    auto new_end=new_begin;
+    try
+    {
+        new_end=orange_stl::uninitialized_move(begin_, pos, new_begin);
+        data_allocator::construct(orange_stl::address_of(*new_end), orange_stl::forward<Args>(args)...);
+        ++new_end;
+        new_end=orange_stl::uninitialized_move(pos, end_, new_end);
+    }
+    catch(...)
+    {
+        data_allocator::deallocate(new_begin, new_size);
+        throw;
+    }
+    destroy_and_recover(begin_, end_, cap_-begin_);
+    begin_=new_begin;
+    end_=new_end;
+    cap_=new_begin+new_size;
+}
+
+
+/* resinert */
+template<class T>
+void vector<T>::reinsert(size_type size)
+{
+    auto new_begin = data_allocator::allocate(size);
+    try{
+        orange_stl::uninitialized_move(begin_, end_, new_begin);
+    }
+    catch(...)
+    {
+        data_allocator::deallocate(new_begin, size);
+        throw;
+    }
+    data_allocator::deallocate(begin_, cap_-begin_);
+    begin_=new_begin;
+    end_=begin_+size;
+    cap_=begin_+size;
+}
+
+/* destroy_and_recover */
+template<class T>
+void vector<T>::destroy_and_recover(iterator first, iterator last, size_type n)
+{
+    data_allocator::destroy(first, last);
+    data_allocator::deallocate(first, n);
+}
+
+/* get_new_cap函数 */
+template <class T>
+typename vector<T>::size_type
+vector<T>::get_new_cap(size_type add_size)
+{
+    const auto old_size = capacity();
+    THROW_LENGTH_ERROR_IF(old_size>max_size()-add_size, "vector<T>'s size too big");
+    if(old_size > max_size() - old_size/2)
+    {
+        return old_size+add_size>max_size-16?old_size+add_size:old_size+add_size+16;
+    }
+    const size_type new_size= old_size==0
+        ?orange_stl::max(add_size, static_cast<size_type>(16))
+        :orange_stl::max(old_size+old_size/2, old_size+add_size);
+    return new_size;
+}
 
 }
 
